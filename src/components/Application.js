87b52de6d69828c784104cd451d870148e08331b
Sydney Sisco <hello@sydsis.co>
2021-03-16 15:49:51 -0700

    use Object referencing

--- src/components/Application.js
+++ src/components/Application.js
@@ -97,15 +97,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {
-          appointments.map((appt) => {
-            return (
-              <Appointment
-                key={appt.id}
-                time={appt.time}
-                interview={appt.interview}
-              />
-            )
-          })
+          appointments.map((appointment) => <Appointment key={appointment.id} {...appointment} />)
         }
         <Appointment key="last" time="5pm" />
       </section>
