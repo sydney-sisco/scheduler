1c11ee088cde744628287bab5669ddd1c95e0e76
Sydney Sisco <hello@sydsis.co>
2021-03-17 15:09:26 -0700

    add default value to transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,7 +4,7 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode, replace) => {
+  const transition = (newMode, replace = false) => {
     setMode(newMode);
 
     if(replace) {
