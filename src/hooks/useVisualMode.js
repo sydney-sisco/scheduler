31b396455017602400b076c6255c4be345f02a3c
Sydney Sisco <hello@sydsis.co>
2021-03-17 14:08:11 -0700

    add 2nd test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,5 +3,7 @@ import React, { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return { mode };
+  const transition = newMode => setMode(newMode);
+
+  return { mode, transition };
 };
