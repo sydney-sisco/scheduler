96a3ef681516a20d9e9ddbb074902f1b78751fc5
Sydney Sisco <hello@sydsis.co>
2021-03-17 16:07:15 -0700

    use custom hook to conditionally display modules

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,21 +4,25 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header.js"
 import Show from "components/Appointment/Show"
 import Empty from "components/Appointment/Empty"
+import useVisualMode from "hooks/useVisualMode";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      { 
-        props.interview ?
+        {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+        {mode === SHOW && (
           <Show
             student={props.interview.student}
             interviewer={props.interview.interviewer}
-          /> :
-          <Empty />
-      }
+          />
+        )}
     </article>
   );
 };
