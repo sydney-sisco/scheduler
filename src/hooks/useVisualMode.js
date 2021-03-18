8855f7317e3617cda60c859194d151a21da58642
Sydney Sisco <hello@sydsis.co>
2021-03-18 13:14:25 -0700

    remove commented code

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,12 +1,9 @@
 import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
-  // const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   const transition = (newMode, replace = false) => {
-    // setMode(newMode);
-
     if(replace) {
       setHistory(prev => [...prev.slice(0, -1), newMode]);
     } else {
